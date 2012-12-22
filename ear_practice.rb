9d812ab420b4d68ba0f257b986775a82b7752d62
Anderson Mesquita <andersonvom@gmail.com>
2012-12-22 17:37:53 -0500

    Added all major and minor chords

--- ear_practice.rb
+++ ear_practice.rb
@@ -5,25 +5,29 @@ class Chords
 
   @@chords = {
       :A  => %w(     A2  E3  A3  C#3 E4  ),
-      :B  => %w(  ),
-      :C  => %w(  ),
+      :B  => %w(     B2  F#3 B3  D#3 F#4 ),
+      :C  => %w(     C3  E3  G3  C4  E4  ),
       :D  => %w(         D3  A3  D4  F#4 ),
       :E  => %w( E2  B2  E3  G#3 B3  E4  ),
-      :F  => %w(  ),
-      :G  => %w(  ),
+      :F  => %w( F2  C3  F3  A3  C4  F4  ),
+      :G  => %w( G2  B2  D3  G3  B3  G4  ),
 
       :Am => %w(     A2  E3  A3  C3  E4  ),
-      :Bm => %w(  ),
-      :Cm => %w(  ),
+      :Bm => %w(     B2  F#3 B3  D3  F#4 ),
+      :Cm => %w(     C3  G3  C4  D#3 G4  ),
       :Dm => %w(         D3  A3  D4  F4  ),
       :Em => %w( E2  B2  E3  G3  B3  E4  ),
-      :Fm => %w(  ),
-      :Gm => %w(  ),
+      :Fm => %w( F2  C3  F3  G#3 C4  F4  ),
+      :Gm => %w( G2  D3  G3  A#3 D4  G4  ),
     }
 
   def self.method_missing(m, *args, &block)
     return @@chords[m] if @@chords.keys.include? m
     super
+  end
+
+  def self.all_chords
+    @@chords
   end
 
 end
