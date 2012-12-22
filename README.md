d37ff5ce6e415f4dbafdcb0d8f35a8d914ebdd67
Anderson Mesquita <andersonvom@gmail.com>
2012-12-22 16:53:08 -0500

    Initial revision with some major and minor chords available

---
+++ README.md
@@ -0,0 +1,25 @@
+Ear Training
+============
+
+This is a little script to help train your ear for guitar chords. Once you select which chords to practice, it will randomly play a chord and ask you to answer what the chord was.
+
+Usage
+=====
+
+    ruby ear_practice.rb 
+    What chords do you want to practice?
+    A Am E Em D Dm
+    How many times?
+    5
+
+    Prepare to play and hit ENTER...
+
+    Enter chord name: A
+    Enter chord name: A
+    Enter chord name: D
+    Enter chord name: E
+    Enter chord name: D
+
+    You scored ** 4/5 **
+
+
---
+++ ear_practice.rb
@@ -0,0 +1,85 @@
+#!/usr/bin/ruby
+
+class Chords
+  attr_accessor :chords
+
+  @@chords = {
+      :A  => %w(     A2  E3  A3  C#3 E4  ),
+      :B  => %w(  ),
+      :C  => %w(  ),
+      :D  => %w(         D3  A3  D4  F#4 ),
+      :E  => %w( E2  B2  E3  G#3 B3  E4  ),
+      :F  => %w(  ),
+      :G  => %w(  ),
+
+      :Am => %w(     A2  E3  A3  C3  E4  ),
+      :Bm => %w(  ),
+      :Cm => %w(  ),
+      :Dm => %w(         D3  A3  D4  F4  ),
+      :Em => %w( E2  B2  E3  G3  B3  E4  ),
+      :Fm => %w(  ),
+      :Gm => %w(  ),
+    }
+
+  def self.method_missing(m, *args, &block)
+    return @@chords[m] if @@chords.keys.include? m
+    super
+  end
+
+end
+
+class EarPractice
+  attr_accessor :training_set
+
+  def initialize(*chords)
+    set = chords
+    set = chords.shift if chords.size == 1
+    raise "You must pass an array of notes to practice" unless set.is_a? Array
+    self.training_set = set
+  end
+
+  def play(chord, interval = 0.05)
+    notes = Chords.send(chord)
+    intervals = notes.size.times.map { |i| interval * i }
+    system "play -q -n synth pl #{notes.join " pl "} delay #{intervals.join " "} remix - fade 0 4 .1 norm -1"
+  end
+
+  def run(num_times = 10)
+    score = 0
+    play_thread = nil
+    begin
+      num_times.times do
+        chord = training_set.sample
+        play_thread = fork { 4.times { play chord } }
+        print "Enter chord name: "
+        guessed_chord = gets.chomp
+        score += 1 if guessed_chord == chord
+        Process.kill "TERM", play_thread
+        play_thread = nil
+        sleep 2
+      end
+    rescue SignalException
+      Process.kill "TERM", play_thread if play_thread
+    end
+    puts
+    puts "You scored ** #{score}/#{num_times} **"
+  end
+
+end
+
+if $0 == __FILE__
+  puts "What chords do you want to practice?"
+  chord_string = gets.chomp
+  chords = chord_string.scan /[A-G]m?/
+
+  puts "How many times?"
+  num_times = gets.to_i
+
+  e = EarPractice.new chords
+  puts "Prepare to play and hit ENTER..."
+  puts
+  gets
+
+  e.run num_times
+end
+
